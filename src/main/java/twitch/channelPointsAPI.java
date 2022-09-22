b694e3671f8bb8cfd60d6881aa263fbc2f5bd7d3
Buchi21 <felixbuchi21@outlook.de>
2022-09-22 20:53:15 +0200

    Deleted unused code

--- src/main/java/twitch/channelPointsAPI.java
+++ src/main/java/twitch/channelPointsAPI.java
@@ -2,25 +2,14 @@ package twitch;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import io.github.cdimascio.dotenv.Dotenv;
-import org.apache.hc.client5.http.ClientProtocolException;
 import org.apache.hc.client5.http.classic.methods.HttpPatch;
-import org.apache.hc.client5.http.classic.methods.HttpPost;
 import org.apache.hc.client5.http.impl.classic.CloseableHttpClient;
 import org.apache.hc.client5.http.impl.classic.CloseableHttpResponse;
 import org.apache.hc.client5.http.impl.classic.HttpClientBuilder;
-import org.apache.hc.core5.http.HttpStatus;
-import org.apache.hc.core5.http.io.entity.EntityUtils;
 import org.apache.hc.core5.http.io.entity.StringEntity;
 import org.apache.hc.core5.net.URIBuilder;
-import org.json.simple.parser.ParseException;
 import utils.GetValuesfromJSON;
 
-import java.io.IOException;
-import java.io.OutputStream;
-import java.net.*;
-import java.nio.charset.StandardCharsets;
-import java.security.KeyStore;
-import java.util.HashMap;
 import java.util.LinkedHashMap;
 import java.util.Map;
 
@@ -49,29 +38,9 @@ public class channelPointsAPI {
                 + "&id=" + redemptionID;
         cancelRewardRedemptionRequest(requestURL, accessToken);
     }
-    //TODO: Fix Patch request
     private void cancelRewardRedemptionRequest(String requestURL, String accessToken) throws Exception {
-        /*HttpURLConnection http = (HttpURLConnection) requestURL.openConnection();
-        http.setRequestProperty("X-HTTP-Method-Override", "PATCH");
-        http.setRequestMethod("POST");
-        http.setDoOutput(true);
-        http.setRequestProperty("client-id", clientID);
-        http.setRequestProperty("Authorization", "Bearer " + accessToken);
-        http.setRequestProperty("Content-Type", "application/json");
-
-        String data = "{\"status\": \"CANCELED\"}";
-
-        byte[] out = data.getBytes(StandardCharsets.UTF_8);
-
-        OutputStream stream = http.getOutputStream();
-        stream.write(out);
-
-        System.out.println(http.getResponseCode() + " " + http.getResponseMessage());
-        http.disconnect();*/
 
         final CloseableHttpClient httpClient = HttpClientBuilder.create().build();
-
-        final ObjectMapper objectMapper = new ObjectMapper();
 
         URIBuilder builder = new URIBuilder(String.valueOf(requestURL));
 
@@ -93,9 +62,6 @@ public class channelPointsAPI {
 
         try (final CloseableHttpResponse response = httpClient.execute(request)) {
             System.out.println(response.getCode());
-
-            /*final Map<String, String> obj = new HashMap<String, String>();
-            System.out.println(objectMapper.readValue(EntityUtils.toString(response.getEntity()), obj.getClass()));*/
 
         } catch (Exception ignored) {
 
